00bfe06c2cf82ddf8c6688488baa1a5f8c4dc26f
Bayrakdar <bayrakdar@Bayrakdar-MacBook-Air.local>
2018-12-14 21:47:40 +0300

    Flight Reservation

---
+++ flight_reservetion.cpp
@@ -0,0 +1,102 @@
+/*
+*   Yusuf Sabri Bayrakdar
+*   2016400378
+*   Operating System Project 2
+*
+*   To compile => 
+*/
+
+/*Libriries*/
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <mutex>
+/*End Of The Definitions*/
+
+/*Definitions*/
+#define project Operating_System_Project_2
+using namespace std;
+pthread_t tid; /* the thread identifier */
+pthread_attr_t attr; /* set of attributes for the thread */
+void *client(void *param); /* the declaration function executed by the thread */
+void *servant(void *param); /* the declaration function executed by the thread */
+int coming_order=1;
+mutex m;
+long totalSeat=0;
+/*End Of The Definitions*/
+
+int main(int argc,char *argv[]){
+    totalSeat = strtol(argv[1], NULL, 10);//Get total seat number
+    if (argc != 2) { // If there is not just one argument, exit
+        fprintf(stderr,"usage: a.out <integer value>\n");
+        return -1;
+    }
+    if(totalSeat<50||totalSeat>100){//If total seat number is not between 50 and 100, exit
+        cout<<"Enter a number between 50 and 100 please!"<<endl;
+        return -1;
+    }
+    cout<<"Welcome to Fight Reservation"<<endl;
+    ofstream out ("output.txt");//Create output.txt
+    out<<"Number of total seats: "<<totalSeat<<endl;//Write total seat number to output.txt
+    int seats[totalSeat+1][2];//Create 2D array for seats and check whether they are full or not,If their second number is 0 then seat is available,If not seat is full.
+    //Create thread list
+    for(int i = 1; i < totalSeat+1; i++)//Fill the seats 2D array as all seats are available;so 0 means available, otherwise it is full.
+    {
+        seats[i][0]=i;
+        seats[i][1]=0;
+    }
+    for(int i = 0; i < totalSeat+1; i++)
+    {
+        if (i!=totalSeat-1) {
+            pthread_attr_init(&attr);/* get the default attributes defined by pthread */
+            pthread_create(&tid,&attr,client,argv[1]);/* create the thread with giving an ID, default attributes, where to start and argument for the method */
+
+            
+        }
+    }
+    
+    while(coming_order!=totalSeat+1){
+        pthread_join(tid,NULL);/* parent thread waits for the child thread to exit */      
+    }
+    
+    
+    
+    
+    
+    // cout<<"argv "<<totalSeat<<endl;
+    return 0;
+}
+
+
+// The execution of the thread will begin in this function
+void *client(void *param) 
+{
+    int sleep=rand()%150+50;//Find a random number between 50-200 to sleep
+    usleep(sleep*1000);//Sleep a few milisec
+    int reserv_seat=rand()%totalSeat;//Choose a seat to reserve after wake up
+    
+    m.lock();
+    string myName="Client "+to_string(coming_order);
+    coming_order++;
+    cout<<myName<<" sleep "<<sleep<<" seat "<<reserv_seat<<endl;
+
+    pthread_attr_init(&attr);/* get the default attributes defined by pthread */
+    pthread_create(&tid,&attr,servant,param);/* create the thread with giving an ID, default attributes, where to start and argument for the method */
+    pthread_join(tid,NULL);/* parent thread waits for the child thread to exit */    
+    
+    m.unlock();
+	pthread_exit(0); // terminate the thread
+}
+
+// The execution of the thread will begin in this function
+void *servant(void *param) 
+{
+    cout<<" I am the servant thread of client "<<to_string(coming_order-1)<<endl;
+	pthread_exit(0); // terminate the thread
+}
