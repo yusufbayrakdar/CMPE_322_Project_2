e317ff9ab4394af23511bf2a891d867b3f4d1d57
Bayrakdar <bayrakdar@Bayrakdar-MacBook-Air.local>
2018-12-19 21:43:09 +0300

    Bişeye benzedi gibi :/

--- flight_reservetion.cpp
+++ flight_reservetion.cpp
@@ -6,7 +6,7 @@
 *   To compile => 
 */
 
-/*Libriries*/
+/*Libraries*/
 #include <iostream>
 #include <vector>
 #include <fstream>
@@ -17,8 +17,8 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <mutex>
-#include "seat.h"
-/*End Of The Definitions*/
+#include <list>
+/*End Of The Libraries*/
 
 /*Definitions*/
 #define project Operating_System_Project_2
@@ -29,10 +29,11 @@ void *client(void *param); /* the declaration function executed by the thread */
 void *servant(void *param); /* the declaration function executed by the thread */
 int coming_order=1;
 mutex m;
+vector<int> seats; 
 long totalSeat=0;
-vector<seat> seat_list;
 int reserv_seat;
-bool reThink=false;
+string myName="Client "+to_string(coming_order);
+ofstream out ("output.txt");//Create output.txt
 /*End Of The Definitions*/
 
 int main(int argc,char *argv[]){
@@ -46,36 +47,27 @@ int main(int argc,char *argv[]){
         return -1;
     }
     cout<<"Welcome to Fight Reservation"<<endl;
-    ofstream out ("output.txt");//Create output.txt
+    
     out<<"Number of total seats: "<<totalSeat<<endl;//Write total seat number to output.txt
     //Create thread list
-    for(int i = 1; i < totalSeat+1; i++)//Fill the seats 2D array as all seats are available;so 0 means available, otherwise it is full.
+    for(int i = 0; i < totalSeat; i++)//Fill the seats 2D array as all seats are available;so 0 means available, otherwise it is full.
     {
-        string client_name="Client "+to_string(i);
-        seat new_seat;
-        new_seat.available=false;
-        new_seat.name="Client "+to_string(i);
-        seat_list.push_back(new_seat);
+        // string client_name="Client "+to_string(i);
+        seats.push_back(0);
+        // cout<<"Seat "<<i+1<<" "<<seats[i]<<endl;
     }
     for(int i = 0; i < totalSeat+1; i++)
     {
         if (i!=totalSeat-1) {
             pthread_attr_init(&attr);/* get the default attributes defined by pthread */
             pthread_create(&tid,&attr,client,argv[1]);/* create the thread with giving an ID, default attributes, where to start and argument for the method */
-
-            
         }
     }
-    
     while(coming_order!=totalSeat+1){
         pthread_join(tid,NULL);/* parent thread waits for the child thread to exit */      
     }
+    out<<"All seats are reserved";
     
-    
-    
-    
-    
-    // cout<<"argv "<<totalSeat<<endl;
     return 0;
 }
 
@@ -85,34 +77,29 @@ void *client(void *param)
 {
     int sleep=rand()%150+50;//Find a random number between 50-200 to sleep
     usleep(sleep*1000);//Sleep a few milisec
-    
-    
     m.lock();
-    reserv_seat=rand()%totalSeat;//Choose a seat to reserve after wake up
-    string myName="Client "+to_string(coming_order);
-    coming_order++;
-    cout<<myName<<" sleep "<<sleep<<" seat "<<reserv_seat<<endl;
+    reserv_seat=rand()%(totalSeat-1);//Choose a seat to reserve after wake up
+    reserv_seat++;
+    myName="Client "+to_string(coming_order);
+    
+    while(true){
+        if (seats[reserv_seat]==0) {
+            seats[reserv_seat]=coming_order;
+            break;
+        }
+        reserv_seat=rand()%totalSeat;//Choose a seat to reserve after wake up
+    }
+    cout<<myName<<" "<<sleep<<" "<<reserv_seat+1<<endl;
         pthread_attr_init(&attr);/* get the default attributes defined by pthread */
         pthread_create(&tid,&attr,servant,param);/* create the thread with giving an ID, default attributes, where to start and argument for the method */
-        pthread_join(tid,NULL);/* parent thread waits for the child thread to exit */ 
-
-       
-    
+        pthread_join(tid,NULL);/* parent thread waits for the child thread to exit */   
     m.unlock();
 	pthread_exit(0); // terminate the thread
 }
-
 // The execution of the thread will begin in this function
 void *servant(void *param) 
 {
-    cout<<" I am the servant thread of client "<<to_string(coming_order-1)<<" my client seat is "<<reserv_seat<<endl;
-    if(seat_list[reserv_seat].available==false)
-        seat_list[reserv_seat].available=true;
-    
-    else
-    {
-        cout<<" Reject"<<endl;
-    }
-    
+    out<<myName<<" reserves "<<reserv_seat+1<<endl;
+    coming_order++;
 	pthread_exit(0); // terminate the thread
 }
